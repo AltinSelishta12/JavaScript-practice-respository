6c75d290857c452144412dcb6604931829074314
Altin Selishta <AltinSelishta14@gmail.com>
2025-10-09 21:52:25 +0200

    Saving the DOM and loop practice in JS

--- .vscode/index.js
+++ .vscode/index.js
@@ -1,5 +1,21 @@
+const container = document.querySelector(".container");
+const items = document.querySelectorAll(".item");
+const Clickbtn = document.querySelector("#Clickbtn");
 
 
+
+for(let i = 0; i <= 4; i++){
+    const box = document.createElement("div");
+    container.appendChild(box);
+    box.classList.add("item");
+}
+
+Clickbtn.addEventListener('click', function(){
+    const items = document.querySelectorAll(".item");
+    items.forEach(item => {
+        item.classList.toggle("active");
+    })
+})
 
 
 
