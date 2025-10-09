b7422ad3832bba61238dd3f3b32c3f58e2b06437
Altin Selishta <AltinSelishta14@gmail.com>
2025-10-09 23:06:14 +0200

    Saving a simple form validation

--- .vscode/index.js
+++ .vscode/index.js
@@ -1,39 +1,35 @@
-const Form = document.querySelector('#Form');
-const TextInput = document.querySelector("#TextInput");
-const addBtn = document.querySelector("#addBtn");
-const list = document.querySelector(".list");
-const resetBtn = document.querySelector(".resetBtn");
-
-
-Form.addEventListener("submit", function(e){
-     e.preventDefault();
-   if(TextInput.value.trim() === ""){
-     e.preventDefault();
-     TextInput.classList.add("active");
-
-     setTimeout(() =>{
-        TextInput.classList.remove("active");
-     }, 1000);
-     return
-   }
-})
-
- addBtn.addEventListener("click", function(e){
-    e.preventDefault();
-    const item = document.createElement("li");
-    list.appendChild(item);
-
-    item.textContent = TextInput.value;
-
-    TextInput.value = "";
-})
-
-resetBtn.addEventListener("click", function(){
-    list.innerHTML = "";
-})
-const item = document.createElement("li");
-item.textContent = TextInput.value;
+const Form = document.querySelector("#Form");
+const Name = document.querySelector("#Name");
+const Age = document.querySelector("#Age");
+const ShowText = document.querySelector("#ShowText");
 
 
 
+Form.addEventListener("submit", function(event){
+    event.preventDefault();
+
+    if(Name.value.trim() === ""){
+        Name.classList.add("active");
+
+        setTimeout(() =>{
+            Name.classList.remove("active");
+        }, 2000);
+        return
+    }
+
+    if(Age.value < 18){
+       ShowText.textContent = "You are still a minor please change your age";
+
+        setTimeout(() =>{
+            ShowText.textContent = "";
+        }, 3000);
+    }
+    else if(Age.value >= 18){
+        ShowText.textContent = "Login rules completed and form is accepted";
+        ShowText.style.color = "green";
+        setTimeout(() =>{
+             ShowText.textContent = "";
+        }, 3000)
+    }
+});
 
