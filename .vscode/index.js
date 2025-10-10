7d88761c313d0178a07b6e34d3b40b5493822946
Altin Selishta <AltinSelishta14@gmail.com>
2025-10-10 16:16:43 +0200

    Saving the light/night mode

--- .vscode/index.js
+++ .vscode/index.js
@@ -1,35 +1,12 @@
-const Form = document.querySelector("#Form");
-const Name = document.querySelector("#Name");
-const Age = document.querySelector("#Age");
-const ShowText = document.querySelector("#ShowText");
+//Js practice
 
+const btn = document.querySelector("#btn");
+const moon = document.querySelector("#moon");
+const sun = document.querySelector("#sun");
 
+btn.addEventListener("click", function(){
+    document.body.classList.toggle("active");
 
-Form.addEventListener("submit", function(event){
-    event.preventDefault();
-
-    if(Name.value.trim() === ""){
-        Name.classList.add("active");
-
-        setTimeout(() =>{
-            Name.classList.remove("active");
-        }, 2000);
-        return
-    }
-
-    if(Age.value < 18){
-       ShowText.textContent = "You are still a minor please change your age";
-
-        setTimeout(() =>{
-            ShowText.textContent = "";
-        }, 3000);
-    }
-    else if(Age.value >= 18){
-        ShowText.textContent = "Login rules completed and form is accepted";
-        ShowText.style.color = "green";
-        setTimeout(() =>{
-             ShowText.textContent = "";
-        }, 3000)
-    }
+    sun.classList.toggle("active");
+    moon.classList.toggle("hide")
 });
-
