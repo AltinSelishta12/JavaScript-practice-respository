f6ecf3d40e5fbf92ecc7fe0e34bdf560a2885230
Altin Selishta <AltinSelishta14@gmail.com>
2025-10-10 17:22:23 +0200

    Saving the add and delete card function

--- .vscode/index.js
+++ .vscode/index.js
@@ -1,12 +1,36 @@
 //Js practice
 
-const btn = document.querySelector("#btn");
-const moon = document.querySelector("#moon");
-const sun = document.querySelector("#sun");
+const container = document.querySelector(".container");
+const clickBtn = document.querySelector("#clickBtn");
+const addBtn = document.querySelector("#addBtn");
+const deleteBtn = document.querySelector("#deleteBtn");
+for(let i = 0; i <= 2; i++){
+    const box = document.createElement("div");
+    box.classList.add("active");
+    container.appendChild(box);
+}
 
-btn.addEventListener("click", function(){
-    document.body.classList.toggle("active");
+clickBtn.addEventListener("click", function(){
+    const boxes = document.querySelectorAll(".active");
 
-    sun.classList.toggle("active");
-    moon.classList.toggle("hide")
-});
+    boxes.forEach(box =>{
+      box.classList.toggle("change");  
+    })
+})
+
+addBtn.addEventListener("click", function(){
+    const box = document.createElement("div");
+    box.classList.add("active");
+    container.appendChild(box);
+})
+
+deleteBtn.addEventListener("click", function(){
+    const boxes = document.querySelectorAll(".active");
+     
+    if(boxes.length > 0){
+        boxes[0].remove();
+    }
+    else{
+        alert("All cards deleted");
+    }  
+})
