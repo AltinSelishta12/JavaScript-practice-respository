cdbb76eb211590c0435f0d42db6df575acd01d6f
Altin Selishta <AltinSelishta14@gmail.com>
2025-10-15 23:29:30 +0200

    Saving JS practice

--- .vscode/index.js
+++ .vscode/index.js
@@ -1,26 +1,11 @@
-//Js practice
-const Form = document.querySelector("#Form");
-const Comment = document.querySelector("#Comment");
-const addTask = document.querySelector("#addTask");
-const list = document.querySelector(".list");
-const removeTask = document.querySelector("#removeTask");
+const addBtn = document.querySelector("#addBtn");
 
-Form.addEventListener("submit", function(event){
+let result = 1;
 
-     if(Comment.value.trim() === ""){
-          event.preventDefault();
-          alert("Please fill the Comment field");
-          return;
-     }
-});
+addBtn.addEventListener("click", function(){
+    result = result - 2;
 
-addTask.addEventListener("click", function(){
-     const li = document.createElement("li");
-     list.appendChild(li);
-     li.classList.add("active");
+    document.getElementById("result").innerHTML = result;
+})
 
-    li.textContent = Comment.value;
 
-    Comment.value = "";
-
-});
